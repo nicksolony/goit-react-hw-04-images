ca1ea738b37e7678ca927b53e36efddb387d1035
nicksolony <nicksolony@gmail.com>
2023-10-06 15:12:23 -0400

    cleaned gh-pages

--- src/components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { MoreButton } from "./Button.styled";
-
-export const Button = ({ loadMore }) => {
-    return (
-        <MoreButton onClick={ loadMore }>Load More</MoreButton>
-    );
-};
